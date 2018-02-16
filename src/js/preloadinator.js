762048aaff57d3ee421a715bd4912ee1c0753ce0
P1x3l5M4773r <pixelsmatter1@gmail.com>
2018-02-16 13:46:32 +0000

    Can now set min time on preloader
    Allows any animations to complete before removing the preloader

--- src/js/preloadinator.js
+++ src/js/preloadinator.js
@@ -3,9 +3,11 @@
 		'use strict';
 
 		var settings = $.extend({
-			scroll: false
+			scroll: false,
+			minTime: 0
 		}, options),
-		preloader = this;
+		preloader = this,
+		start = new Date().getTime();
 
 		function disableScroll() {
 			$('body').css('overflow', 'hidden');
@@ -21,12 +23,30 @@
 			});			
 		}
 
-		if(settings.scroll === false) {
-			disableScroll();
+		function minTimeElapsed() {
+			var now = new Date().getTime(),
+			elapsed = now - start;
+
+			if(elapsed >= settings.minTime) {
+				return true;
+			}
+			else {
+				return false;
+			}
 		}
 
+		if(settings.scroll === false) { disableScroll(); }
+
 		$(window).on('load', function() {
-			removePreloader();
+			if(minTimeElapsed()) {
+				removePreloader();
+			}
+			else {
+				var now = new Date().getTime(),
+				elapsed = now - start;
+
+				setTimeout(removePreloader, settings.minTime - elapsed);
+			}
 		});
 
 	    return this;
