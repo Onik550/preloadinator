e1c76b9cd277ac72b875b3926fb109048b4791b8
P1x3l5M4773r <pixelsmatter1@gmail.com>
2018-02-16 11:20:14 +0000

    Scrolling disabled when preloader is shown
    Prevents users aimlessly scrolling page whilst it is loading

--- src/js/preloadinator.js
+++ src/js/preloadinator.js
@@ -3,11 +3,28 @@
 		'use strict';
 
 		var settings = $.extend({
+			scroll: false
 		}, options),
 		preloader = this;
 
+		function disableScroll() {
+			$('body').css('overflow', 'hidden');
+		}
+
+		function enableScroll() {
+			$('body').css('overflow', 'auto');
+		}
+
+		if(settings.scroll === false) {
+			disableScroll();
+		}
+
 		$(window).on('load', function() {
-			$(preloader).fadeOut();
+			$(preloader).fadeOut(function() {
+				if(settings.scroll === false) {
+					enableScroll();
+				}
+			});
 		});
 
 	    return this;
